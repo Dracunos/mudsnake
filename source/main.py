3d68f3685a0e76bd2d9bcb8bc6bce29119beeaa8
Dracunos <dracunos72@hotmail.com>
2018-10-16 14:56:48 -0700

    Changed filename extension and started off with some comments

---
+++ source/main.py
@@ -0,0 +1,10 @@
+"""This program will connect to MUD servers via telnet.
+ Hopefully we will provide some useful functions like triggers,
+aliases, macros, and maybe even some scripting for your mud
+experience.
+ I will either need to write an entire domain specific language
+or use something like embedded lua.
+"""
+
+
+
