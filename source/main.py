1234777738f02fa740b6d387c4e1afe68c760779
Dracunos <dracunos72@hotmail.com>
2018-10-24 09:44:33 -0700

    Added basic telnet functionality

--- source/main.py
+++ source/main.py
@@ -2,9 +2,40 @@
  Hopefully we will provide some useful functions like triggers,
 aliases, macros, and maybe even some scripting for your mud
 experience.
- I will either need to write an entire domain specific language
-or use something like embedded lua.
 """
 
+import telnethandler
+import threading
 
+def main():
+    connection = telnethandler.TelnetHandler("aardwolf.org", 23)
+    connection.start()
+    def f():
+        while True:
+            line = connection.output_queue.get()
+            print(line.decode("ascii"))
+    t = threading.Thread(target=f)
+    t.start()
+    while True:
+        inn = input("> ")
+        connection.send(inn)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
 
---
+++ source/telnethandler.py
@@ -0,0 +1,34 @@
+"""This module will handle telnet connections and IO.
+
+"""
+
+import telnetlib
+import queue
+import threading
+
+class TelnetHandler:
+    
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.connection = telnetlib.Telnet(host, port)
+        self.output_queue = queue.Queue()
+
+    def read_line(self):
+        line = self.connection.read_until(b"\n")
+        self.output_queue.put(line)
+
+    def send(self, userInput):
+        self.connection.write((userInput + "\n").encode("ascii"))
+
+    def run(self):
+        while True:
+            self.read_line()
+
+    def start(self):
+        self.thread = threading.Thread(target=self.run)
+        self.thread.start()
+
+
+
+
